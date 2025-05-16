f713329c9b4f715a01b325f86ee99e0421970450
CliffLinusthomi <linusthomi2@gmail.com>
2025-05-16 23:15:17 +0300

    first commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
+file_name = "/Development/code/se-prep/PHASE-3/python-p3-fileio-lab/file_learn"
+file_content = "This is a test content."
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", 'r') as file:
+        return file.read()
